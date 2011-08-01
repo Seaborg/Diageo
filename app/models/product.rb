0dad08811ccf73aa1590f99b0d5e7218807b9ff5
Your Name <Seaborg@SC.local>
2011-08-01 12:19:50 +0100

    BasicLayout

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
+#---
+# Excerpted from "Agile Web Development with Rails, 4rd Ed.",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+  validates :title, :description, :image_url, :presence => true
+# 
+  validates :image_url, :format => {
+    :with    => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
