41fa116caa39264cdf277c3de4e24190b02d49c6
Your Name <Seaborg@SC.local>
2011-08-01 12:06:11 +0100

    Oxwood Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oxwood::Application.config.secret_token = 'cc3188084cd67845ce29977aad3bf006e6506f00e1956e28937a95a9db3e3c8298a8070ea89ab36270e827c0ed9d24ee4ce062a08e77781e0acb3af42a3cdb58'
